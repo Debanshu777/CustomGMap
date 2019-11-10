f6d029a36f2be128b47843150268fb4df71c8a06
Debanshu777 <debanshudatta123@gmail.com>
2019-11-10 12:14:26 +0530

    Dark mode updated

--- app/src/main/java/com/example/custommapview/MapsActivity.java
+++ app/src/main/java/com/example/custommapview/MapsActivity.java
@@ -2,11 +2,18 @@ package com.example.custommapview;
 
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Color;
+import android.location.Geocoder;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -14,82 +21,70 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.GoogleMapOptions;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.Locale;
+
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
-    Button dark;
+    Geocoder geocoder;
+    Switch dark;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        dark=findViewById(R.id.dark);
+        dark=findViewById(R.id.switch1);
 
 
     }
-
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(final GoogleMap googleMap) {
 
         mMap = googleMap;
+        geocoder = new Geocoder(this, Locale.getDefault());
         final Marker[] marker = new Marker[1];
-        final CameraUpdate cameraupdate;
-        dark.setOnClickListener(new View.OnClickListener() {
+
+        dark.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                try {
-                    // Customise the styling of the base map using a JSON object defined
-                    // in a raw resource file.
-                    boolean success = googleMap.setMapStyle(
-                            MapStyleOptions.loadRawResourceStyle(
-                                    getApplicationContext(), R.raw.mapstyle));
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked) {
+                        try {
+                            // Customise the styling of the base map using a JSON object defined
+                            // in a raw resource file.
+                            boolean success = googleMap.setMapStyle(MapStyleOptions.loadRawResourceStyle(getApplicationContext(), R.raw.mapstyle));
 
-                    if (!success) {
-                        Log.e("MapActivity", "Style parsing failed.");
-                    }
-                } catch (Resources.NotFoundException e) {
-                    Log.e("MapActivity", "Can't find style. Error: ", e);
+                            if (!success) {
+                                Log.e("MapActivity", "Style parsing failed.");
+                            }
+                        } catch (Resources.NotFoundException e) {
+                            Log.e("MapActivity", "Can't find style. Error: ", e);
+                        }
+
                 }
+                else {
+                    try {
+                        boolean success = googleMap.setMapStyle(
+                                MapStyleOptions.loadRawResourceStyle(
+                                        getApplicationContext(), R.raw.mystyle2));
 
+                        if (!success) {
+                            Log.e("MapActivity", "Style parsing failed.");
+                        }
+                    } catch (Resources.NotFoundException e) {
+                        Log.e("MapActivity", "Can't find style. Error: ", e);
+                    }
+                }
             }
         });
-       try {
-            // Customise the styling of the base map using a JSON object defined
-            // in a raw resource file.
-            boolean success = googleMap.setMapStyle(
-                    MapStyleOptions.loadRawResourceStyle(
-                            this, R.raw.mystyle2));
-
-            if (!success) {
-                Log.e("MapActivity", "Style parsing failed.");
-            }
-        } catch (Resources.NotFoundException e) {
-            Log.e("MapActivity", "Can't find style. Error: ", e);
-        }
-        // Add a marker in Sydney and move the camera
-        final LatLng sydney = new LatLng(-34, 151);
-        marker[0] =mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
         googleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
 
             @Override
